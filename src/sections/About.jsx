14a82728f112f33c40caa5e7b138163a81043b32
Eduardo <el_alienx@hotmail.com>
2023-09-26 17:02:29 +0200

    added css code for about section but introduce a img bug

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -1,14 +1,31 @@
+// Project files
+import Image from "../assets/about.png";
+
 export default function About() {
   return (
     <section className="about">
       <div className="container">
         <h2>About</h2>
-        <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Libero odio
-          ut aut incidunt est! Laboriosam totam vero eligendi nulla, facere sit
-          veritatis ratione quo ex sunt, reiciendis cupiditate perspiciatis
-          libero!
-        </p>
+        <div className="content">
+          <div>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Libero
+              odio ut aut incidunt est! Laboriosam totam vero eligendi nulla,
+              facere sit veritatis ratione quo ex sunt, reiciendis cupiditate
+              perspiciatis libero!
+            </p>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Libero
+              odio ut aut incidunt est! Laboriosam totam vero eligendi nulla,
+              facere sit veritatis ratione quo ex sunt, reiciendis cupiditate
+              perspiciatis libero!
+            </p>
+          </div>
+          <img
+            src={Image}
+            alt="A blue all in one computer with some decoration elements"
+          />
+        </div>
       </div>
     </section>
   );
