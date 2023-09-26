33eadde5cbd1ef65da76845059b5c6b0b57a1c90
Eduardo <el_alienx@hotmail.com>
2023-09-26 15:24:22 +0200

    started to build the skeleton

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
+// Project files
+import Footer from "./components/Footer";
+import NavigationBar from "./components/NavigationBar";
+import About from "./sections/About";
+import Contact from "./sections/Contact";
+import Hero from "./sections/Hero";
+import Projects from "./sections/Projects";
+import Technologies from "./sections/Technologies";
+
 export default function App() {
   return (
     <div className="App">
-      <h1>Portfolio website</h1>
+      <NavigationBar />
+      <Hero />
+      <About />
+      <Projects />
+      <Technologies />
+      <Contact />
+      <Footer />
     </div>
   );
 }
