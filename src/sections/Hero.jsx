6b8a2107317e327746699b3c93101dc1b3b76db7
Eduardo <el_alienx@hotmail.com>
2023-09-26 16:14:28 +0200

    added hero design

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,7 +1,17 @@
 export default function Hero() {
   return (
     <section className="hero">
-      <h1>I'm Eduardo</h1>
+      <h1>Welcome to Eduardo's portfolio</h1>
+      <span className="subtitle">
+        You just found a detail oriented backend developer
+      </span>
+      {/* Some tips: */}
+      {/* This is one 1 but i want to stylize by showing it in 2 lines use <br/> */}
+      {/* h1: Hi, I'm Eduardo frontend developer from Egypt. */}
+
+      {/* If is actually 2 sentences use a span tag with classname subtitle */}
+      {/* h1: Eduardo Alvarez. */}
+      {/* span.subtitle The best frontend developer in town. */}
     </section>
   );
 }
