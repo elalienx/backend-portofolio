29811932807a5fdbe54bfff6011c13cf54cd27f5
Eduardo <el_alienx@hotmail.com>
2023-09-27 16:43:02 +0200

    fix image

--- src/components/ItemProject.jsx
+++ src/components/ItemProject.jsx
@@ -2,9 +2,9 @@ export default function ItemProject({ item }) {
   const { image_thumb_url, title } = item;
 
   return (
-    <article className="item-project">
+    <button className="item-project">
       <img src={image_thumb_url} />
       <h3>{title}</h3>
-    </article>
+    </button>
   );
 }
