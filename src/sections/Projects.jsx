e7eee3edb61bb2b92d9555c4066beb6d89da0829
Eduardo <el_alienx@hotmail.com>
2023-09-27 15:54:29 +0200

    i have create the information date

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -3,6 +3,13 @@ export default function Projects() {
     <section className="projects">
       <div className="container">
         <h2>Projects</h2>
+        <div className="content">
+          <p>
+            Here are the projects I will be making during the frontend course at
+            Novare potential.
+          </p>
+          <div className="grid">{/* The thumbnails of the projects */}</div>
+        </div>
       </div>
     </section>
   );
