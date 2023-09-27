683fb0401c82f4c1a4dcc2046e06ecf707e5c335
Eduardo <el_alienx@hotmail.com>
2023-09-27 16:00:41 +0200

    added the component item project, now going to add CSS

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -1,4 +1,15 @@
+// Project files
+import Data from "../data/projects.json";
+
 export default function Projects() {
+  // Components
+  const Items = Data.map((item) => (
+    <article key={item.id}>
+      <img src={item.image_thumb_url} />
+      <h3>{item.title}</h3>
+    </article>
+  ));
+
   return (
     <section className="projects">
       <div className="container">
@@ -8,7 +19,7 @@ export default function Projects() {
             Here are the projects I will be making during the frontend course at
             Novare potential.
           </p>
-          <div className="grid">{/* The thumbnails of the projects */}</div>
+          <div className="grid">{Items}</div>
         </div>
       </div>
     </section>
