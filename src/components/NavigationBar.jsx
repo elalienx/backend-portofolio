a5c5022fb768cf047d0144112343a90f2936688e
Eduardo <el_alienx@hotmail.com>
2023-09-29 12:40:01 +0200

    added icons

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -1,8 +1,18 @@
+// Node modules
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { icon } from "@fortawesome/fontawesome-svg-core/import.macro";
+
 export default function NavigationBar() {
   return (
     <nav className="navigation-bar">
       <div className="container">
-        <span className="name">Eduardo Alvarez</span>
+        <div className="logo">
+          <FontAwesomeIcon
+            className="icon"
+            icon={icon({ name: "rocket", style: "solid" })}
+          />
+          <span className="name">Eduardo Alvarez</span>
+        </div>
         <div className="links">
           <a href="#about">About</a>
           <a href="#projects">Projects</a>
