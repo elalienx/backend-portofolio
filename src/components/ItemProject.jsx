5212f357f8d944f19e63e2ffd32ef82f6d6001ed
Eduardo <el_alienx@hotmail.com>
2023-10-02 15:46:03 +0200

    design fixes

--- src/components/ItemProject.jsx
+++ src/components/ItemProject.jsx
@@ -5,12 +5,12 @@ export default function ItemProject({ item, setModal }) {
   const { image_thumb_url, title } = item;
 
   return (
-    <button
+    <article
       onClick={() => setModal(<ModalProject item={item} />)}
       className="item-project"
     >
       <img src={image_thumb_url} />
       <h3>{title}</h3>
-    </button>
+    </article>
   );
 }
