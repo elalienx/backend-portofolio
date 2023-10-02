b4bede99354dd3099ab62ae33b1fcc1a047ce785
Eduardo <el_alienx@hotmail.com>
2023-10-02 16:20:44 +0200

    implemented icons

--- src/components/ItemTechnology.jsx
+++ src/components/ItemTechnology.jsx
@@ -1,8 +1,12 @@
+// Node modules
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 export default function ItemTechnology({ item }) {
-  const { title } = item;
+  const { icon_style, icon_name, title } = item;
 
   return (
     <article className="item-technology">
+      <FontAwesomeIcon icon={[icon_style, icon_name]} />
       <h3>{title}</h3>
     </article>
   );
