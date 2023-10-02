179dbe136a00872514f0eb9a2c35cf52b1965091
Eduardo <el_alienx@hotmail.com>
2023-10-02 15:05:53 +0200

    i remove stuff not in use right now

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
 // Project files
 import { useState } from "react";
-import Footer from "./components/Footer";
 import Modal from "./components/Modal";
-import NavigationBar from "./components/NavigationBar";
 import About from "./sections/About";
-import Contact from "./sections/Contact";
 import Hero from "./sections/Hero";
 import Projects from "./sections/Projects";
-import Technologies from "./sections/Technologies";
 import "./styles/style.css";
 
 export default function App() {
@@ -16,13 +12,9 @@ export default function App() {
 
   return (
     <div className="App">
-      <NavigationBar />
       <Hero />
       <About />
       <Projects setModal={setModal} />
-      <Technologies />
-      <Contact />
-      <Footer />
       {/* Modal invisible unless someone sents a React component using setModal() */}
       <Modal state={[modal, setModal]} />
     </div>
