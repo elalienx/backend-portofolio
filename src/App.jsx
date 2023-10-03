567dda701a2476e438325f0e55b13a81a5604ee3
Eduardo <el_alienx@hotmail.com>
2023-10-03 09:58:52 +0200

    Reorder imports in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,13 @@ import { useState } from "react";
 
 // Project files
 import Modal from "./components/Modal";
-import NavigationBar from "./components/NavigationBar";
 import About from "./sections/About";
+import Contact from "./sections/Contact";
 import Hero from "./sections/Hero";
 import Projects from "./sections/Projects";
 import Technologies from "./sections/Technologies";
-import "./styles/style.css";
 import "./scripts/fontawesome";
+import "./styles/style.css";
 
 export default function App() {
   // Local state
