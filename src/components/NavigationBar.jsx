4a4145aa240aacd561e866832c6a3b0a9e556d09
Eduardo <el_alienx@hotmail.com>
2023-10-03 16:34:18 +0200

    we finish the code and polish of the navigation bar

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -1,20 +1,31 @@
 // Node modules
+import { faL } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useState } from "react";
 
 export default function NavigationBar() {
+  // Local state
+  const [showLinks, setShowLinks] = useState(false);
+
+  // Properties
+  const cssToggle = showLinks ? "show" : "hide";
+
   return (
     <nav className="navigation-bar">
       {/* Main menu 🔴 */}
       <div className="main-menu">
         <FontAwesomeIcon className="logo icon" icon={["fas", "fa-rocket"]} />
-        <button className="menu-button">
+        <button
+          onClick={() => setShowLinks(!showLinks)}
+          className="menu-button"
+        >
           <FontAwesomeIcon className="icon" icon={["fas", "fa-bars"]} />
         </button>
         <span className="name">Eduardo Alvarez</span>
       </div>
 
       {/* Links 🟢 */}
-      <div className="links">
+      <div className={`links ${cssToggle}`}>
         <a href="#about">About</a>
         <a href="#projects">Projects</a>
         <a href="#technologies">Technologies</a>
